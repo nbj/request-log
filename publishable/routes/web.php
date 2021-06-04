c6907302d3edbebd0f87c9a9750a8f3a5fd69cca
Thomas Wogelius <thwo@cego.dk>
2021-06-04 13:58:10 +0200

    Removed csrf check

--- publishable/routes/web.php
+++ publishable/routes/web.php
@@ -2,20 +2,23 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::namespace('Cego\RequestLog\Controllers')->prefix('vendor')->group(function () {
-    Route::get('request-logs/toggle', 'RequestLogController@toggle')
-        ->name('request-logs.toggle')
-        ->middleware('web');
+Route::namespace('Cego\RequestLog\Controllers')->prefix('vendor')
+    ->middleware([
+        \App\Http\Middleware\EncryptCookies::class,
+        \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+        \Illuminate\Session\Middleware\StartSession::class,
+        \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+        \Illuminate\Routing\Middleware\SubstituteBindings::class,
+    ])->group(function () {
+        Route::get('request-logs/toggle', 'RequestLogController@toggle')
+            ->name('request-logs.toggle');
 
-    Route::delete('request-logs/delete', 'RequestLogController@delete')
-        ->name('request-logs.delete')
-        ->middleware('web');
+        Route::delete('request-logs/delete', 'RequestLogController@delete')
+            ->name('request-logs.delete');
 
-    Route::resource('request-logs/blacklisted-routes', 'BlacklistedRoutesController')
-        ->only(['index', 'create', 'store', 'destroy'])
-        ->middleware('web');
+        Route::resource('request-logs/blacklisted-routes', 'BlacklistedRoutesController')
+            ->only(['index', 'create', 'store', 'destroy']);
 
-    Route::resource('request-logs', 'RequestLogController')
-        ->only(['index', 'show', 'destroy'])
-        ->middleware('web');
-});
+        Route::resource('request-logs', 'RequestLogController')
+            ->only(['index', 'show', 'destroy']);
+    });
