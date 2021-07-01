163dff2ec885bb2a19ee54c8d2ede47607ff8e51
niza <niza@cego.dk>
2021-07-01 13:41:48 +0200

    Added type hint

--- src/RequestLog/Commands/AutomaticLogCleanup.php
+++ src/RequestLog/Commands/AutomaticLogCleanup.php
@@ -29,7 +29,7 @@ class AutomaticLogCleanup extends Command
      *
      * @return int
      */
-    public function handle()
+    public function handle(): int
     {
         $isEnabled = Config::get('request-log.automaticLogCleanUpEnabled', false);
 
