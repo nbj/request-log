cf81b44c5c1e1fa6a08ae7f0d35f02ad2b7caf45
Niki Ewald Zakariassen <niza@cego.dk>
2021-08-11 11:43:42 +0200

    Added migration to increase string length

---
+++ publishable/migrations/2021_08_11_114137_increase_string_column_length.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class IncreaseStringColumnLength extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::table('request_logs', static function (Blueprint $table) {
+            $table->string('query', 2000)->change();
+            $table->string('url', 2000)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::table('request_logs', static function (Blueprint $table) {
+            $table->string('query')->change();
+            $table->string('url')->change();
+        });
+    }
+}
